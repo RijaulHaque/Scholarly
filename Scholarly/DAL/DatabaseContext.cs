f63ded338112dbf14d492366ab7674966c25dbaf
HP <HP@LAPTOP-MBDV8QJG>
2025-01-06 12:17:19 +0530

    Add project files.

---
+++ Scholarly/DAL/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Scholarly.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Scholarly.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<users> user { get; set; }
+    }
+}
---
+++ Scholarly/Models/users.cs
@@ -0,0 +1,15 @@
+﻿namespace Scholarly.Models
+{
+    public class users
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string role_id  { get; set; }
+
+    }
+}
