5c0ec906de33cb2ff22eec589af792a0cf1a6184
RijaulHaque <project.rijaulhaque@gmail.com>
2025-02-06 21:13:24 +0530

    Modified the Dynamic Background and some other stuffs

--- Scholarly/Controllers/HomeController.cs
+++ Scholarly/Controllers/HomeController.cs
@@ -1,164 +1,4 @@
-//using System.Diagnostics;
-//using Microsoft.AspNetCore.Mvc;
-//using Scholarly.Models;
 
-//namespace Scholarly.Controllers
-//{
-//    public class HomeController : Controller
-//    {
-//        private readonly ILogger<HomeController> _logger;
-
-//        public HomeController(ILogger<HomeController> logger)
-//        {
-//            _logger = logger;
-//        }
-
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-
-//        public IActionResult Privacy()
-//        {
-//            return View();
-//        }
-
-//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//        public IActionResult Error()
-//        {
-//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//        }
-
-
-
-//        // Add Student Enrollment
-//        [HttpPost]
-//        public IActionResult AddEnrollment(int studentId, int courseId)
-//        {
-//            // Logic to add enrollment
-//            // Example: Save to database
-//            return Ok("Enrollment added successfully");
-//        }
-
-//        // Add Student Attendance
-//        [HttpPost]
-//        public IActionResult AddAttendance(int studentId, int courseId, bool isPresent)
-//        {
-//            // Logic to add attendance
-//            // Example: Save attendance record
-//            return Ok("Attendance recorded successfully");
-//        }
-
-//        // Add Teacher Grade
-//        [HttpPost]
-//        public IActionResult AddGrade(int studentId, int courseId, string grade)
-//        {
-//            // Logic to add grade
-//            // Example: Save grade record
-//            return Ok("Grade added successfully");
-//        }
-
-//        // Add Teacher Attendance
-//        [HttpPost]
-//        public IActionResult AddTeacherAttendance(int teacherId, int courseId, bool isPresent)
-//        {
-//            // Logic to add teacher attendance
-//            // Example: Save teacher attendance record
-//            return Ok("Teacher attendance recorded successfully");
-//        }
-
-//    }
-//}
-
-//####################### NEW CODE 16 JAN #######################
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using Scholarly.DAL;
-//using Scholarly.Models;
-//using System.Linq;
-//using System.Threading.Tasks;
-
-//namespace Scholarly.Controllers
-//{
-//    public class HomeController : Controller
-//    {
-//        private readonly DatabaseContext _context;
-
-//        public HomeController(DatabaseContext context)
-//        {
-//            _context = context;
-//        }
-
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-
-//        public IActionResult Privacy()
-//        {
-//            return View();
-//        }
-
-//        public async Task<IActionResult> StudentDashboard()
-//        {
-//            var user = await _context.user
-//                .Where(u => u.Username == User.Identity.Name)
-//                .FirstOrDefaultAsync();
-
-//            if (user == null)
-//            {
-//                return NotFound();
-//            }
-
-//            var student = await _context.student
-//                .Where(s => s.Name == user.Username)
-//                .FirstOrDefaultAsync();
-
-//            if (student == null)
-//            {
-//                return NotFound();
-//            }
-
-//            var enrollments = await _context.Enrollments
-//                .Where(e => e.StudentId == student.Id)
-//                .Include(e => e.Courses)
-//                .Include(e => e.Attendance)
-//                .ToListAsync();
-
-//            var model = new StudentDashboardViewModel
-//            {
-//                StudentId = student.Id,
-//                Username = user.Username,
-//                Email = user.Email,
-//                Year = student.Year,
-//                Enrollments = enrollments
-//            };
-
-//            return View(model);
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> AddAttendance(int enrollmentId, int studentId, bool isPresent)
-//        {
-//            var attendance = new Attendance
-//            {
-//                Date = DateTime.Now,
-//                EnrollmentsId = enrollmentId,
-//                AttendanceData = isPresent
-//            };
-
-//            _context.Attendance.Add(attendance);
-//            await _context.SaveChangesAsync();
-
-//            return RedirectToAction("StudentDashboard");
-//        }
-
-//        public IActionResult Error()
-//        {
-//            return View();
-//        }
-//    }
-//}
 
 //######### 21st Jan code ################
 using Microsoft.AspNetCore.Mvc;
@@ -191,13 +31,18 @@ namespace Scholarly.Controllers
 
         public async Task<IActionResult> StudentDashboard()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Login"); // Redirect to login if not authenticated
+            }
+
             var user = await _context.user
                 .Where(u => u.Username == User.Identity.Name)
                 .FirstOrDefaultAsync();
 
             if (user == null)
             {
-                return NotFound();
+                return RedirectToAction("Index", "Login"); // Redirect to login if user doesn't exist
             }
 
             var student = await _context.student
@@ -206,7 +51,7 @@ namespace Scholarly.Controllers
 
             if (student == null)
             {
-                return NotFound();
+                return RedirectToAction("Index", "Login"); // Redirect if student not found
             }
 
             var enrollments = await _context.Enrollments
@@ -216,9 +61,11 @@ namespace Scholarly.Controllers
                 .ToListAsync();
 
             var model = new StudentDashboardViewModel(student);
-
             return View(model);
         }
+
+
+
 
         public IActionResult Error()
         {
--- Scholarly/Models/LoginViewModel.cs
+++ Scholarly/Models/LoginViewModel.cs
@@ -13,19 +13,3 @@ namespace Scholarly.Models
     }
 }
 
-
-
-//######################################### OLD CODE ##########################################//
-
-//public class LoginModel
-//{
-//        //[Required(ErrorMessage = "Username is required.")]
-//[Display(Name = "Username")]
-//public string Username { get; set; } = string.Empty;
-
-//[Required(ErrorMessage = "Password is required.")]
-//[DataType(DataType.Password)]  // Hides password as typed
-//[Display(Name = "Password")]
-//public string Password { get; set; } = string.Empty;
-
-
